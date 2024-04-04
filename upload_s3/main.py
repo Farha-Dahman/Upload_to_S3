fe5447bc2afd7737bac08700b242d9f3915c9db3
Farha Dahman <farhajehad@gmail.com>
2024-04-04 15:22:08 +0300

    added environment file

--- upload_s3/main.py
+++ upload_s3/main.py
@@ -1,13 +1,23 @@
 import sys
 import os
+from dotenv import load_dotenv
 current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(current_dir)
 sys.path.append(root_dir)
 from upload_s3.upload_to_s3 import upload_file_to_s3
 
 def main():
-    file_path = 'receipts\\receipt.txt'
-    bucket_name = 'receipts-storage-pal'
+    # Load environment variables from .env file
+    load_dotenv()
+
+    # Get file path and bucket name from environment variables
+    file_path = os.getenv("FILE_PATH")
+    bucket_name = os.getenv("BUCKET_NAME")
+
+    if file_path is None or bucket_name is None:
+        print("Error: Please make sure FILE_PATH and BUCKET_NAME are set in the .env file.")
+        return
+    
 
     if upload_file_to_s3(file_path, bucket_name):
         print("File uploaded successfully.")
